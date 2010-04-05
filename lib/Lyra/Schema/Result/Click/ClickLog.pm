c058e031408ab9f59695e8531506a5d40b5abea6
lestrrat <lestrrat+github@gmail.com>
2010-04-05 17:51:39 +0900

    add a schema for logging clicks

---
+++ lib/Lyra/Schema/Result/Click/ClickLog.pm
@@ -0,0 +1,25 @@
+package Lyra::Schema::Result::Click::ClickLog;
+use strict;
+use warnings;
+use base qw(Lyra::Schema::Result);
+
+__PACKAGE__->engine( 'QUEUE' );
+__PACKAGE__->load_components( qw(TimeStamp Core) );
+__PACKAGE__->table('lyra_click_log');
+__PACKAGE__->add_columns(
+    ad_id => {
+        data_type => 'CHAR',
+        size      => 36,
+        is_nullable => 0,
+    },
+    data => {
+        data_type => 'TEXT',
+        is_nullable => 0,
+    },
+    created_on => {
+        data_type => 'DATETIME',
+        set_on_create => 1,
+    }
+);
+
+1;
